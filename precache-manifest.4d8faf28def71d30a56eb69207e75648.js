4e9da996d090c9e9097d7cc2664c2bdf6a8886e4
bmoffitt1990 <bmoffitt1990@gmail.com>
2019-08-20 22:12:18 -0400

    Updates

---
+++ precache-manifest.4d8faf28def71d30a56eb69207e75648.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d387177610eafec183e79f2c3d8371ee",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "8c205f3be292a431fc29",
+    "url": "/monsters-rolodex/static/css/main.536199dc.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "8c205f3be292a431fc29",
+    "url": "/monsters-rolodex/static/js/main.75d5ac52.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
